6d79dd13b4094491ef2441d5214705d04f28dad2
Mykhol <michaelcoryhowell@outlook.com>
2020-05-20 20:14:16 +1200

    [Master] Test
    This is a test commit.

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>You are a poop.</p>
           <a
             className="App-link"
             href="https://reactjs.org"
